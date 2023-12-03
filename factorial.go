c0fc9370279b8d1bdc703b13f7b666d42e756a8f
obinna franklin <franklinokeke2016@gmail.com>
2023-12-03 15:25:58 +0100

    factorial code

---
+++ factorial.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+func factorialCalculate(x int) int{
+	return  5*4*3*2*1
+	// return result
+	
+}
+
+func main(){
+
+        // var result int
+
+     factorialCalculate(5)
+	fmt.Println("the factorial of 5 is:",factorialCalculate(5))
+	// fmt.Scan(&number)
+
+
+
+}
